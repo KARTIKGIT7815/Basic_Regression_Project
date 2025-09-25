db44429c8ef48bf3079de18497f56aa7f2c0c36b
unknown <kartikshingate90@gmail.com>
2025-09-25 20:49:04 +0530

    Final

--- app.py
+++ app.py
@@ -24,6 +24,30 @@ scaler = pickle.load(open("models/scaler.pkl", "rb"))
 def hello_world():
     return render_template('index.html')
 
+@app.route("/predictdata", methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method=="POST":
+        form_data = request.form.to_dict()
+        for key, value in form_data.items():  
+            try:
+                form_data[key] = float(value)  # Convert if it's a valid number
+            except ValueError:
+                pass  
+        
+        df = pd.DataFrame([form_data])
+        print(df)
+        
+        S_scaled = scaler.transform(df[df.columns])
+
+        print('Scaling_Value',S_scaled)
+        yp = model.predict(S_scaled)
+
+
+        return render_template('home.html',result=yp[0])
+    
+
+    else:
+        return render_template('home.html')
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
